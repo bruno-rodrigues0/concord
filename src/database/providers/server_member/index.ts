b894ff48d0700131f08d84ceeaba710f8df50a2b
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2026-01-11 14:10:30 -0300

    Revert "implements some server controllers"
    This reverts commit ff058b8cc3c10b9335353d5f70f6d162376fcc3d.

--- src/database/providers/server_member/index.ts
+++ src/database/providers/server_member/index.ts
@@ -1,15 +1,7 @@
 import { create } from "./create";
-import { del } from "./delete";
-import { getAllMembersByServer } from "./getAllMembersByServer";
-import { getAllServerByMember } from "./getAllServersByMember";
 import { getById } from "./getById";
-import { update } from "./update";
 
 export const serverMemberProvider = {
   create,
   getById,
-  getAllMembersByServer,
-  getAllServerByMember,
-  del,
-  update,
 };
